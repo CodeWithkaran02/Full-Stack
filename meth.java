78acb111985639d7f4be89f6bf1c69f6fbdbb728
CodeWithkaran02 <7870karuna@gmail.com>
2023-09-22 23:15:01 +0530

    methid

---
+++ meth.java
@@ -0,0 +1,19 @@
+public class meth {
+    void mth() {
+        int a = 10;
+        System.out.println(10);
+        System.out.println(20);
+        System.out.println(a + 20);
+    }
+
+    void mth2() {
+        System.out.println(111);
+    }
+
+    public static void main(String args[]) {
+        meth obj = new meth();
+        // mth.obj();
+
+        System.out.println("java is amazing");
+    }
+}
