5631268ec2d9b02242075e0f243cd8fe19009f57
CodeWithkaran02 <7870karuna@gmail.com>
2023-09-29 09:49:51 +0530

    method with two another case

---
+++ classA2.java
@@ -0,0 +1,19 @@
+public class classA2 {
+    void meth1() {
+        System.out.println("hii");
+
+    }
+
+    void meth2() {
+        System.out.println("hello");
+
+    }
+
+    public static void main(String[] args) {
+        classA2 aobj = new classA2();
+        aobj.meth1();
+        aobj.meth2();
+
+    }
+
+}
