81555b92e42095c067a2e1728ab14627396cdd8d
CodeWithkaran02 <7870karuna@gmail.com>
2023-10-02 22:51:31 +0530

    what is diff b/w #includ #difine and import

---
+++ import/ClassX.java
@@ -0,0 +1,13 @@
+package pack2;
+
+import pack1.ClassA1;
+import pack1.ClassB;
+
+public class classX {
+
+    public static void main(String[] args) {
+        new ClassA1().meth1();
+        new ClassB().meth2();
+    }
+
+}
