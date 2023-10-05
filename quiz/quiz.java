dea6259beca47ff27d6f71992e7599282d56b39c
CodeWithkaran02 <7870karuna@gmail.com>
2023-10-05 15:52:33 +0530

    quiz

---
+++ quiz/quiz.java
@@ -0,0 +1,6 @@
+class quiz {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i * i--);
+    }
+}
---
+++ quiz/quiz1.java
@@ -0,0 +1,10 @@
+
+public class quiz1 {
+    public static void main(String[] args) {
+        String word = "hello world";
+
+        for (int i = 0; i <= 5; i++)
+            System.out.print(word.charAt(i));
+    }
+
+}
