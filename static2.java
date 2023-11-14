336a42b182414f1e940e4ed6e8eb6dc2feb14471
CodeWithkaran02 <7870karuna@gmail.com>
2023-11-14 15:52:06 +0530

    static prority

--- static2.java
+++ static2.java
@@ -1,5 +1,25 @@
 public class static2 {
-    public static void main(String[] args) {
 
+    static int x = meth1();
+
+    static {
+        System.out.println("1st static block executed: " + x);
+    }
+
+    static int meth1() {
+        System.out.println("methiod1 call");
+        return 100;
+
+    }
+
+    static int y = meth2();
+
+    static int meth2() {
+        System.out.println("meth2()  callrd");
+        return 200;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("main() called");
     }
 }
