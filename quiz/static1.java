c581901bb5a5ab1fc4fe391ecc6460266b209edd
CodeWithkaran02 <7870karuna@gmail.com>
2023-11-14 16:36:31 +0530

    statuc quzie

---
+++ quiz/static1.java
@@ -0,0 +1,11 @@
+public class static1 {
+    public static void main(String[] args) {
+        int x = 20;
+        System.out.println(x);
+    }
+
+    static {
+        int x = 10;
+        System.out.println(x + " ");
+    }
+}
