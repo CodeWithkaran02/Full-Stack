435e5b91fb56394271ebe6ba75c421b0af0db4b9
CodeWithkaran02 <7870karuna@gmail.com>
2023-11-15 11:00:39 +0530

    staic methiod and static block

--- static1.java
+++ static1.java
@@ -1,9 +1,30 @@
 public class static1 {
-    void meth() {
-        System.out.println("hello");
+
+    static int x = 10;
+    static int y = 20;
+    static {
+
+        System.out.println("---test1 sb1 called ----");
+        System.out.println("X: " + x);
+        System.out.println("Y: " + y);
+    }
+
+    static void ml() {
+        int x = 100;
+
+        System.out.println("static method called");
+        System.out.println("x:" + x);
+        System.out.println("y:" + y);
+
     }
 
     public static void main(String[] args) {
+        ml();
+    }
 
+    static {
+        System.out.println("test2 sb2 called ");
+        System.out.println("x:" + x);
+        System.out.println("y: " + y);
     }
 }
