18d12b24b38b24f1fe2996cb9da98e73bd34d44e
CodeWithkaran02 <7870karuna@gmail.com>
2023-11-25 18:43:27 +0530

    output

---
+++ quiz/quize5.java
@@ -0,0 +1,21 @@
+public class quize5 {
+    void display() {
+        byte x;
+        int a = 270;
+        double d = 128.128;
+        x = (byte) a;
+        System.out.println("a and x " + a + " " + x);
+
+        a = (int) b;
+
+        System.out.println("b and a " + b + " " + a);
+        x = b;
+        System.out.println("b and x " + b + " " + x);
+
+    }
+
+    public static void main(String[] args) {
+        quize5 q = new quize5();
+        q.display();
+    }
+}
