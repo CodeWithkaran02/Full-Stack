3bc8e842e9e06de4238c08373af69b0c3ab6974e
CodeWithkaran02 <7870karuna@gmail.com>
2023-12-15 13:42:06 +0530

    substring

--- String/qurry3.java
+++ String/qurry3.java
@@ -1,12 +1,13 @@
 public class qurry3 {
     public String makeOutWorld(String out, String word) {
-        return out.concat(word).concat(">>");
+        // return out.concat(word).concat(">>");
         // return "<<" + word + ">>";
+        return out.substring(0, 2) + word + out.substring(2);
     }
 
     public static void main(String[] args) {
         qurry3 q3obj = new qurry3();
-        String result = q3obj.makeOutWorld("<<", "Yay");
+        String result = q3obj.makeOutWorld("<<>>", "Yay");
         System.out.println(result);
 
     }
