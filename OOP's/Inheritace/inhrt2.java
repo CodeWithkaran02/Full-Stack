428e17712f19f0a02d389c4356081d0a5c043100
CodeWithkaran02 <7870karuna@gmail.com>
2023-12-24 22:19:11 +0530

    super keyword with this keyword

---
+++ OOP's/Inheritace/inhrt2.java
@@ -0,0 +1,12 @@
+public class inhrt2 extends Inhr1 {
+    void display() {
+        System.out.println("Hi");
+        super.meth4();
+
+    }
+
+    public static void main(String[] args) {
+        new inhrt2().display();
+
+    }
+}
