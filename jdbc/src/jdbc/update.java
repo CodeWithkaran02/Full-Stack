fce8eabd0022d9d8d8e06a80bf8b5a169b50ca83
CodeWithkaran02 <7870karuna@gmail.com>
2024-08-30 19:23:59 +0530

    updat sql for java

--- jdbc/src/jdbc/update.java
+++ jdbc/src/jdbc/update.java
@@ -4,21 +4,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-
-public class insert {
+public class update {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","1104");
-			Statement stmt = con.createStatement();
-			stmt.executeUpdate("insert into student values(11,'hhh',90)");
-			System.out.println("One Record Successfully");
+			Connection con= DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","1104");
+			Statement  stmt = con.createStatement();
+			
+			stmt.executeUpdate("update student set marks =55 where rollno=3");
+			
+			System.out.println("One Record update successfully");
 		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		
 	}
 
